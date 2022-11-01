f5ecff18e616e696d814770a360569e9c114f42c
Michael Petter <michael@petterisbetter.com>
2022-11-01 16:43:41 -0500

    reimplement add hero using reactive forms

--- src/app/new-hero/new-hero.component.ts
+++ src/app/new-hero/new-hero.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -9,12 +10,20 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./new-hero.component.scss']
 })
 export class NewHeroComponent {
-  constructor(private heroService: HeroService, private router: Router) { }
+  form: FormGroup;
 
-  add(name: string) {
-    const sanitizedName = name.trim();
-    if(!sanitizedName) return;
-    this.heroService.add({ name: sanitizedName } as Hero)
+  constructor(formBuilder: FormBuilder, private heroService: HeroService, private router: Router) {
+    this.form = formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern(/\S+/)]]
+    });
+  }
+
+  submit() {
+    const hero = {
+      name: this.form.get('name')?.value.trim()
+    };
+
+    this.heroService.add(hero as Hero)
       .subscribe(() => this.router.navigateByUrl('/heroes'));
   }
 }
